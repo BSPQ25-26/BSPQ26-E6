6d704012cbaa053f0a69d9a2179d468bfc2f09d8
Alex Ramarathinam <alex.ramarathinam@opendeusto.es>
2026-04-22 10:54:30 +0200

    Display teams and final score

--- Football_Manager/src/main/java/com/example/football_manager/repository/MatchRepository.java
+++ Football_Manager/src/main/java/com/example/football_manager/repository/MatchRepository.java
@@ -4,10 +4,10 @@ import com.example.football_manager.model.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.OffsetDateTime;
 import java.util.List;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
+    List<Match> findByFinishedTrueOrderByDatetimeDesc();
 }
 
