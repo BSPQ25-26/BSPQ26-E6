d87bc10869b116d07264d4309e38322e829782cf
Alba <145106923+AlbaDelgadoO@users.noreply.github.com>
2026-04-23 10:43:50 +0200

    Match list UI and view controller

--- Football_Manager/src/main/java/com/example/football_manager/controller/MatchViewController.java
+++ Football_Manager/src/main/java/com/example/football_manager/controller/MatchViewController.java
@@ -1,6 +1,9 @@
 package com.example.football_manager.controller;
 
 import com.example.football_manager.dto.MatchRequestDTO;
+import com.example.football_manager.service.MatchService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MatchViewController {
 
+    @Autowired
+    private MatchService matchService;
+
     @GetMapping("/matches/schedule")
     public String showScheduleForm(Model model) {
         model.addAttribute("matchRequest", new MatchRequestDTO());
         return "schedule-match";
+    }
+
+    @GetMapping("/matches")
+    public String showMatchesPage(Model model) {
+        model.addAttribute("matches", matchService.getAllMatches());
+        return "matches"; 
     }
 }
