5126846796677fcc5cacaa98b546a625a4acce70
IkerCastilloAA <iker.c@opendeusto.es>
2026-04-25 14:55:51 +0200

    All tasks done and working

--- Football_Manager/src/main/java/com/example/football_manager/controller/MatchViewController.java
+++ Football_Manager/src/main/java/com/example/football_manager/controller/MatchViewController.java
@@ -2,6 +2,8 @@ package com.example.football_manager.controller;
 
 import com.example.football_manager.dto.MatchRequestDTO;
 import com.example.football_manager.service.MatchService;
+import com.example.football_manager.service.TeamService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,9 @@ public class MatchViewController {
     @Autowired
     private MatchService matchService;
 
+    @Autowired
+    private TeamService teamService;
+
     @GetMapping("/matches/schedule")
     public String showScheduleForm(Model model) {
         model.addAttribute("matchRequest", new MatchRequestDTO());
@@ -28,23 +33,21 @@ public class MatchViewController {
         return "match-results";
     }
     
-    @GetMapping("/matches/edit/{id}")
+@GetMapping("/matches/edit/{id}")
     public String showEditForm(@PathVariable Long id, Model model) {
         MatchRequestDTO matchRequestDTO = matchService.getMatchForEdit(id)
                 .orElseThrow(() -> new IllegalArgumentException("Match not found."));
+        
         model.addAttribute("matchRequest", matchRequestDTO);
         model.addAttribute("matchId", id);
+        // PASAR LA LISTA DE EQUIPOS PARA LOS DESPLEGABLES
+        model.addAttribute("teams", teamService.getAllTeams()); 
         return "edit-match";
     }
 
     @PostMapping("/matches/edit/{id}")
     public String updateMatch(@PathVariable Long id, @ModelAttribute("matchRequest") MatchRequestDTO matchDTO) {
-        try {
-
-            matchService.updateMatch(id, matchDTO);
-            return "redirect:/matches/results";
-        } catch (IllegalArgumentException e) {
-            return "redirect:/matches/edit/" + id + "?error=true";
-        }
+        matchService.updateMatch(id, matchDTO);
+        return "redirect:/matches/results";
     }
 }
